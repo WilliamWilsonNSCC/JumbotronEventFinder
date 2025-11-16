ad64e82d5a8d277be0286853ebeda9c5a6f04fb9
WilliamWilsonNSCC <w0457754@campus.nscc.ca>
2025-11-16 11:05:05 -0400

    test

---
+++ JumbotronEventFinder/Migrations/20251116150308_AlignPurchaseSchema.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JumbotronEventFinder.Migrations
+{
+    public partial class AlignPurchaseSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "Purchases",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+
+            migrationBuilder.RenameColumn(
+                name: "CCV",
+                table: "Purchases",
+                newName: "CVV");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CVV",
+                table: "Purchases",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CVV",
+                table: "Purchases",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.RenameColumn(
+                name: "CVV",
+                table: "Purchases",
+                newName: "CCV");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CardNumber",
+                table: "Purchases",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
