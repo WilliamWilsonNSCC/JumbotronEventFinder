cd8143cde7b509e52667a09d3c7128eb2bbb768f
WilliamWilsonNSCC <w0457754@campus.nscc.ca>
2025-11-16 00:36:45 -0400

    test

---
+++ JumbotronEventFinder/Migrations/20251116043630_DbUpdate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JumbotronEventFinder.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CVV",
+                table: "Purchase",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CVV",
+                table: "Purchase",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
