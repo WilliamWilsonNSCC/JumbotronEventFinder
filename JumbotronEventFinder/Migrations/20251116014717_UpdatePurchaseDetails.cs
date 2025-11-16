072ac8a88a3ca89601eafa9b2b393633757ac69f
WilliamWilsonNSCC <w0457754@campus.nscc.ca>
2025-11-15 21:49:52 -0400

    update migration

---
+++ JumbotronEventFinder/Migrations/20251116014717_UpdatePurchaseDetails.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JumbotronEventFinder.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePurchaseDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CardNumber",
+                table: "Purchase",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CardNumber",
+                table: "Purchase",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
